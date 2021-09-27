ec362ad42d7b568c106a053a79535e7fa6cab356
Gary Burner <grburner@gmail.com>
2021-09-27 16:02:28 -0400

    specfic recipe nav

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,15 +2,42 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        image={itemData.item.imageUrl}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: 'MealDetail',
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+
   const catId = props.navigation.getParam('categoryId');
 
-  const matchedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+  const matchedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
 
   return (
     <View style={styles.screen}>
-      <FlatList data={matchedMeals}/>
+      <FlatList
+        data={matchedMeals}
+        renderItem={renderMealItem}
+        style={{ width: '100%' }}
+      />
     </View>
   );
 };
@@ -18,10 +45,10 @@ const CategoryMealsScreen = (props) => {
 CategoryMealsScreen.navigationOptions = (navigationData) => {
   const catId = navigationData.navigation.getParam('categoryId');
 
-  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return {
-    headerTitle: selectedCategory.title
-  }
+    headerTitle: selectedCategory.title,
+  };
 };
 
 const styles = StyleSheet.create({
@@ -29,6 +56,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    margin: 10,
   },
 });
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { MEALS } from '../data/dummy-data';
+
 const MealDetailScreen = props => {
+  const mealId = props.navigation.getParam('mealId');
+
+  const matchedMeal = MEALS.filter(
+    meal => meal.id === mealId
+  )
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal Detail Screen</Text>
+      <Text>{mealId}</Text>
+      <Text>{matchedMeal[0].title}</Text>
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = navigationData => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const matchedMeal = MEALS.filter(
+    meal => meal.id === mealId
+  )
+  
+  return {
+    headerTitle: matchedMeal[0].title
+  };
 };
 
 const styles = StyleSheet.create({
