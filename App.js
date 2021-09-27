59203d2edaaa7f571b1da21a4764f63ea0fa246a
Gary Burner <grburner@gmail.com>
2021-09-27 11:41:18 -0400

    inital file nav commit

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+import MealsNavigator from './navigation/MealsNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [loaded, error] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!loaded) {
+    return (
+      <AppLoading
+        onError={console.warn}
+    />
+    );
+  }
+
+  return <MealsNavigator />;
 }
 
 const styles = StyleSheet.create({
