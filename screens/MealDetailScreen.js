1e0cf7757b7faf34c3ef41f160c4128e3460c008
Gary Burner <grburner@gmail.com>
2021-09-28 09:34:00 -0400

    adding favorites page

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -25,7 +27,10 @@ MealDetailScreen.navigationOptions = navigationData => {
   )
   
   return {
-    headerTitle: matchedMeal[0].title
+    headerTitle: matchedMeal[0].title,
+    headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton} >
+      <Item title='Favorite' iconName='ios-star' onPress={() => {console.log ('mark as favorite')}} />
+    </HeaderButtons>
   };
 };
 
